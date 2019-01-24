d860065e19dc07bb68096d9e951fe297af7be785
kryptag <Kryptag@gmail.com>
2019-01-24 16:23:16 +0100

    changes

--- src/main/java/com/kryptag/translator/Main.java
+++ src/main/java/com/kryptag/translator/Main.java
@@ -5,6 +5,10 @@
  */
 package com.kryptag.translator;
 
+import com.kryptag.rabbitmqconnector.Enums.ExchangeNames;
+import com.kryptag.rabbitmqconnector.RMQConnection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  *
  * @author florenthaxha
@@ -15,7 +19,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         // Takes request from RMQ and puts in into the internal queue
+        RMQConnection rmqPub = new RMQConnection("guest", "guest", "datdb.cphbusiness.dk", 5672, "Bank_Translator_Exchange_Name");
+        // Takes the request from internal queue and procceses it before sending it back to RMQ
+        RMQConnection rmqCon = new RMQConnection("guest", "guest", "datdb.cphbusiness.dk", 5672, "Bank_Exchange_Name");
+        // Internal queue
+        ConcurrentLinkedQueue q = new ConcurrentLinkedQueue();
+        // producer implementation 
+        Producer producer = new Producer(q, rmqPub);
+        // procceses implementation 
+        Consumer consumer = new Consumer(q, rmqCon);
+        // start thread
+        producer.start();
+        // start thread 
+        consumer.start();
+        
     }
     
 }
--- src/main/java/com/kryptag/translator/Producer.java
+++ src/main/java/com/kryptag/translator/Producer.java
@@ -5,10 +5,30 @@
  */
 package com.kryptag.translator;
 
+import com.kryptag.rabbitmqconnector.RMQConnection;
+import com.kryptag.rabbitmqconnector.RMQProducer;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author florenthaxha
  */
-public class Producer {
+public class Producer extends RMQProducer  {
+    
+    public Producer(ConcurrentLinkedQueue queue, RMQConnection rmq) {
+        super(queue, rmq);
+    }
+    
+     @Override
+    public void run() {
+       try {
+            this.getRmq().putMessageInQueue(this.getQueue());
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(RMQProducer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
